4ba7cd21fdc8abf3db93df7f104e69da2b73c070
wushejun <346371635@qq.com>
2024-10-22 17:48:50 +0800

    add sql file

---
+++ aydtest.sql
@@ -0,0 +1,78 @@
+/*
+SQLyog Ultimate v11.27 (32 bit)
+MySQL - 5.7.26 : Database - aydtest
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`aydtest` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `aydtest`;
+
+/*Table structure for table `role` */
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '角色ID',
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '角色名称',
+  `created_at` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `updated_at` timestamp NULL DEFAULT NULL COMMENT '最后更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `roles_name_unique` (`name`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `role` */
+
+insert  into `role`(`id`,`name`,`created_at`,`updated_at`) values (1,'admin','2024-10-22 11:42:05','2024-10-22 11:42:08'),(2,'a','2024-10-22 13:50:07','2024-10-22 13:50:10'),(3,'b','2024-10-22 13:50:13','2024-10-22 13:50:15'),(4,'c','2024-10-22 13:50:19','2024-10-22 13:50:21'),(5,'d','2024-10-22 13:50:24','2024-10-22 13:50:26');
+
+/*Table structure for table `sql_log` */
+
+DROP TABLE IF EXISTS `sql_log`;
+
+CREATE TABLE `sql_log` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '日志ID',
+  `user_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '用户ID',
+  `executed_at` datetime NOT NULL COMMENT '执行时间',
+  `sql_statement` text COMMENT 'sql语句',
+  `error` text COMMENT '错误信息',
+  `created_at` datetime DEFAULT NULL COMMENT '记录时间',
+  `updated_at` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='sql执行日志表';
+
+/*Data for the table `sql_log` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '用户名',
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '邮箱',
+  `email_verified_at` timestamp NULL DEFAULT NULL COMMENT '邮箱验证时间',
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '密码',
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '记住密码令牌',
+  `role_id` int(10) DEFAULT '0' COMMENT '角色ID',
+  `created_at` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `updated_at` timestamp NULL DEFAULT NULL COMMENT '最后更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`name`,`email`,`email_verified_at`,`password`,`remember_token`,`role_id`,`created_at`,`updated_at`) values (1,'admin','admin@admin.com',NULL,'$2y$12$Sl7poksrreEweXKWeCzWwOAlkSk4m9O7HT6GnWXiP4Y17IP6AlfjC',NULL,1,'2024-10-22 03:41:06','2024-10-22 03:41:06');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
